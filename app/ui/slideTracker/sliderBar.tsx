ae6db86b601c099a531f87002b9a0599fd995059
Roddy <adarkwagodred7075@icloud.com>
2024-02-23 18:41:18 +0000

    slide tracker ui &  animation added

--- app/ui/slideTracker/sliderBar.tsx
+++ app/ui/slideTracker/sliderBar.tsx
@@ -1,23 +1,29 @@
 "use client"
 
-import { useScroll, useSpring , motion } from "framer-motion"
+import { useScroll, useSpring , motion , useTransform , useMotionValueEvent } from "framer-motion"
+import { useEffect, useRef } from "react";
 
 
 export default function SliderBar() {
 
-    const { scrollY } = useScroll()
-    const Y = useSpring(scrollY)
+
+    const { scrollYProgress } = useScroll({})
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+      console.log("Page scroll: ", latest)
+    })
+     
+    const y = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+    
     
 
 
-
-
   return (
-    <div className='w-12 flex-none h-screen absolute right-0 hidden md:flex items-center justify-center px-3'>
-        <div className='w-full h-[50%] bg-black rounded-[40px] p-1'>
-            <motion.div  style={{ y: Y }} className='w-full h-[20%] bg-white rounded-[25px]'></motion.div>
+<div>
+<div className='w-12 flex-none h-screen fixed right-0 hidden md:flex items-center justify-center px-3'>
+        <div className='w-full h-[50%] bg-black rounded-[42px] p-1'>
+            <motion.div  style={{  y }} className='w-full h-[20%] bg-white rounded-[25px]'></motion.div>
         </div>
-
     </div>
+</div>
   )
 }
