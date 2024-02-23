92a9fb6c179922ed8b17dfe3a6a4c27fdd5cec8e
Roddy <adarkwagodred7075@icloud.com>
2024-02-23 18:38:44 +0000

    hero image scale fixed

--- app/ui/hero/hero.tsx
+++ app/ui/hero/hero.tsx
@@ -4,8 +4,7 @@ export default function Hero() {
   return (
     <>
     <div className="w-full h-full overflow-hidden relative">
-        <Image src="/images/cal_2.jpg"  fill={true}  alt="hero" className="Object-contain"/>
-
+        <Image src="/images/cal_2.jpg"  fill={true}  alt="hero" className="object-contain"/>
     </div>
     </>
   )
