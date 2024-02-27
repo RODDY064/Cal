21ffa547145eef0aa329f790eefc4a3ac94d703b
RODDY064 <124875320+RODDY064@users.noreply.github.com>
2024-02-27 20:45:06 +0000

    div added

--- app/ui/discover/slideUp.tsx
+++ app/ui/discover/slideUp.tsx
@@ -10,7 +10,9 @@ export default function SlideUp({ control}:{ control:any} ) {
     <motion.div
     variants={Expand}
     animate={control}
-    className="w-full md:w-[92%] 2xl:w-[80%]  py-2 h-[12rem] md:h-[15rem] lg:h-[10rem] bg-black rounded-[30px]">
+    className="w-full md:w-[92%] 2xl:w-[80%] flex flex-col items-center sm:items-start  sm:flex-row  py-2 
+    h-[12rem] md:h-[15rem] lg:h-[10rem] bg-black  rounded-[30px] p-4 text-white">
+    <div className="w-full h-full"></div>
    </motion.div>
   )
 }
